723b0e666b67cfe3cf1e1819f2bcc81129e79fe6
Arón Yáñez <yanezaron@gmail.com>
2021-07-01 22:56:30 -0500

    Add first Unit Test

---
+++ LoanChainOfResponsibility.Tests/Handlers/OnTimeHandler.HandleTests.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace LoanChainOfResponsibility.Tests.Handlers
+{
+
+    using LoanChainOfResponsibility.Handlers;
+    using LoanChainOfResponsibility.Handlers.Models;
+    using Xunit;
+
+    public partial class OnTimeHandlerTests
+    {
+        [Fact]
+        public void Given_OnTimeNotTrue_When_CallHandle_ShouldReturnDefaultHandler()
+        {
+            // Arrange
+            Loan loan = new()
+            {
+                OnTime = false
+            };
+            var valueExpected = "NOT ON TIME";
+            // Act 
+            var result = this.systemUnderTest.Handle(loan);
+
+            //Assert
+            Assert.Equal(result, valueExpected);
+        }
+    }
+}
