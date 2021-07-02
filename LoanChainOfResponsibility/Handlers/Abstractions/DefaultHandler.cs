6b1d0ab905e691760359b9b87607557b09766dba
Arón Yáñez <yanezaron@gmail.com>
2021-07-01 21:18:27 -0500

    Add handler abstractions

---
+++ LoanChainOfResponsibility/Handlers/Abstractions/DefaultHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLoan.Handlers
+{
+    class DefaultHandler : IHandler
+    {
+        private IHandler _next;
+        public object Handle(object request)
+        {
+            return _next?.Handle(request);
+        }
+
+        public IHandler SetNext(IHandler handler)
+        {
+            _next = handler;
+
+            return _next;
+        }
+    }
+}
